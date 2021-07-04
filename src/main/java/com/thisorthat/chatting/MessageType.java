1b0a46d06904c368c57af61454d022064e2e8c6a
brandon <mkk0618@naver.com>
2021-07-04 13:39:35 +0900

    feat(WebSocket): Implement websocket connection and API

---
+++ src/main/java/com/thisorthat/chatting/MessageType.java
@@ -0,0 +1,6 @@
+package com.thisorthat.chatting;
+
+public enum MessageType {
+    JOIN,
+    CHAT
+}
--- src/main/java/com/thisorthat/chatting/WebChatHandler.java
+++ src/main/java/com/thisorthat/chatting/WebChatHandler.java
@@ -1,5 +1,6 @@
 package com.thisorthat.chatting;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -10,26 +11,39 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.thisorthat.chatting.MessageType.*;
+
 @Component
 @Log4j2
-public class ChatHandler extends TextWebSocketHandler {
+public class WebChatHandler extends TextWebSocketHandler {
+    private static final String TAG = WebChatHandler.class.getSimpleName();
     private static List<WebSocketSession> list = new ArrayList<>();
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        log.info(session + " 클라이언트 접속");
+        log.info(TAG + ".afterConnectionEstablished "  + session + " client connected");
         super.afterConnectionEstablished(session);
+
         list.add(session);
+
+        ChatRoom chatRoom = ChatRoom.getInstance();
+        chatRoom.addParticipant(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info(TAG + ".handleTestMessage " + session + " " + message);
         super.handleTextMessage(session, message);
-        String payload = message.getPayload();
-        log.info("payload : " + payload);
 
-        for(WebSocketSession sess : list){
-            sess.sendMessage(message);
+        ObjectMapper objectMapper = new ObjectMapper();
+        ChatMessage chatMessage = objectMapper.readValue(message.getPayload(), ChatMessage.class);
+
+        if (chatMessage.getMessageType() == MessageType.JOIN) {
+            chatMessage = new ChatMessage("System", MessageType.JOIN, "님이 입장했습니다.");
         }
+
+        ChatRoom.getInstance().sendMessageToAll(chatMessage, objectMapper);
+
     }
 
     @Override
