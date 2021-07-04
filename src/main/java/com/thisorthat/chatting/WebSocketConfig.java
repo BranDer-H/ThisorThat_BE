858666137d35d64e5fad461bc2f88c98a23ce3cf
Minkyun Kim <mkk0618@naver.com>
2021-07-04 13:41:59 +0900

    feat(WebSocket): Implement websocket connection and API (#6)
    closes #5

--- src/main/java/com/thisorthat/chatting/WebSocketConfig.java
+++ src/main/java/com/thisorthat/chatting/WebSocketConfig.java
@@ -1,19 +1,30 @@
 package com.thisorthat.chatting;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@Log4j2
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-    private final ChatHandler chatHandler;
+    private static final String TAG = WebSocketConfig.class.getSimpleName();
+
+    private final WebChatHandler webChatHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler, "/ws/chat").setAllowedOrigins("*");//chatHandler에 대한 endpoint를 /chat으로 설정한다. setAllowedOrigins()는 도메인이 다른 서버에서도 접속 가능하도록 한다.(없애보자)
+        log.info(TAG + "registerWebSocketHandlers");
+
+        /** postman으로 테스트 할 땐 아래 문장 수행 */
+        registry.addHandler(webChatHandler, "/web/chat").setAllowedOrigins("*");
+
+        /** 실제 Front와 통신에는 아래 문장 수행 */
+        //registry.addHandler(webChatHandler, "/web/chat").setAllowedOrigins("*").withSockJS();
+
     }
 }
